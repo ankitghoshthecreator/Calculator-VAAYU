584158c5c02fa192623f913c234541c458483e58
ankitghoshthecreator <baruipara01@gmail.com>
2024-07-04 21:41:33 +0530

    changes

--- app/src/main/java/com/ankitghoshthecreator/calculator/MainActivity.kt
+++ app/src/main/java/com/ankitghoshthecreator/calculator/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -28,7 +29,9 @@ class MainActivity : ComponentActivity() {
 }
         @Composable
         fun MyApp(modifier: Modifier = Modifier) {
-            Text("Hello, Calculator!", modifier = modifier)
+            Column(modifier= Modifier.fillMaxSize()) {
+
+            }
         }
 
         @Preview(showBackground = true)
